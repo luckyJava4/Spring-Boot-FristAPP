7b425fe7050b725c2195b9acdab67d1f48a31182
Lakshmi Narayana <mlanarayana.u4@gmail.com>
2020-07-23 12:19:22 +0530

    CH-232020

---
+++ src/main/java/com/tcs/SpringBootFristAppApplication.java
@@ -0,0 +1,14 @@
+package com.tcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFristAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFristAppApplication.class, args);
+		System.out.println(" HELLO SPRING BOOT DEVLOPER");
+	}
+
+}
